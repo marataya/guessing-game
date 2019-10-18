6bfdbeaa215817fb7dda013316ac9a61bb45d790
marataya <marataya@gmail.com>
2019-10-18 19:14:16 +0600

    guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min=null,max=null) {
+		this.min = min;
+        this.max = max;
+        this._num = null;
+	}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this._num = this.min + Math.round((this.max - this.min)/2);
+        return this._num;
     }
 
     lower() {
-
+		this.max = this._num;
     }
 
     greater() {
-
+		this.min = this._num;
     }
 }
 
